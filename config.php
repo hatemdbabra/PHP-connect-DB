5769037cda956a399a99d9b64824ca8e957c034e
hatemdbabra <31578196+hatemdbabra@users.noreply.github.com>
2017-10-13 19:43:38 +0200

    How to change witch PDO PHP 5.6
    How to change witch PDO PHP 5.6

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$server="mysql.hostinger.ae";
+
+$user="u274458123_0";
+
+$pass="09126367hh";
+
+$base="u274458123_0";
+
+$c=mysql_connect($server,$user,$pass);
+
+mysql_select_db($base);
+
+?>
